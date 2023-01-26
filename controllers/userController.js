8c3e0ea777febd7e3b4cabd9e3f85da8de1044f7
Shaheer Khan NS <khanshaheer43@gmail.com>
2023-01-26 17:43:22 +0530

    Added encryption to password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,17 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
   try {
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
+    const { name, email, password } = req.body;
 
-    await User.create({
-      name,
-      email,
-      password,
-    });
-
-    res.status(201).json({
-      message: "success UserCreated",
+    bcrypt.hash(password, 12, async (err, hash) => {
+      await User.create({
+        name,
+        email,
+        password: hash,
+      });
+      res.status(201).json({ message: "Successfully create new user" });
     });
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -25,22 +23,31 @@ exports.login = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email: email } });
     if (user) {
-      if (user.dataValues.password == password) {
-        res.status(202).json({
-          status: "success",
-          message: "Logged In succesffully",
-        });
-      } else {
-        res.status(401).json({
-          status: "fail",
-          message: "Unauthorized",
-        });
-      }
+      const userPassword = user.dataValues.password;
+      bcrypt.compare(password, userPassword, (err, result) => {
+        if (err) {
+          res.status(500).json({
+            status: "fail",
+            message: "Something went wrong",
+          });
+        } else if (result === true) {
+          res.status(202).json({
+            status: "success",
+            message: "Logged In succesffully",
+          });
+        } else {
+          res.status(401).json({
+            status: "fail",
+            message: "Unauthorized",
+          });
+        }
+      });
+    } else {
+      res.status(404).json({
+        status: "fail",
+        message: "User Not Found",
+      });
     }
-    res.status(404).json({
-      status: "fail",
-      message: "User Not Found",
-    });
   } catch (err) {
     console.log(err.message);
   }
