20f1153512f220497c6e1ab6de57cd3ce1a3fdc4
Shaheer Khan NS <khanshaheer43@gmail.com>
2023-01-26 15:22:19 +0530

    Added a login route in backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,12 +13,33 @@ exports.createUser = async (req, res) => {
     });
 
     res.status(201).json({
-      status: "success",
-      data: {
-        name,
-        email,
-        password,
-      },
+      message: "success UserCreated",
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email: email } });
+    if (user) {
+      if (user.dataValues.password == password) {
+        res.status(202).json({
+          status: "success",
+          message: "Logged In succesffully",
+        });
+      } else {
+        res.status(401).json({
+          status: "fail",
+          message: "Unauthorized",
+        });
+      }
+    }
+    res.status(404).json({
+      status: "fail",
+      message: "User Not Found",
     });
   } catch (err) {
     console.log(err.message);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,5 @@ const userController = require("../controllers/userController");
 const router = express.Router();
 
 router.route("/").post(userController.createUser);
-
+router.route("/login").post(userController.login);
 module.exports = router;
